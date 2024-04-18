d16a2729ba75b0cedbf6d0f6d83ba8bb8b09fa50
Rikikesh <adarshamishra98@gmail.com>
2024-04-18 21:18:15 +0545

    Added Pagination

--- blog/base/urls.py
+++ blog/base/urls.py
@@ -5,5 +5,5 @@ app_name='base'
 urlpatterns = [
     path('home',views.home,name='home'),
     path('list',views.post_list,name='post_list'),
-    path('<int:id>/',views.post_detail,name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail,name='post_detail'),
 ]
--- blog/base/views.py
+++ blog/base/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render
 from django.http import Http404
 from .models import Post
+from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator
+
 # Create your views here.
 def home(request):
     return render(request,'base/post/home.html')
 
-def post_list(request):
-    posts=Post.published.all()
-    return render(request,'base/post/list.html',{'posts':posts})
 
-def post_detail(request,id):
-    try:
-        post=Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found")
+
+def post_list(request):
+    post_list = Post.published.all()
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+    return render(request,
+                  'base/post/list.html',
+                  {'posts': posts})
+
+
+def post_detail(request,year,month,day,post):
+    post=get_object_or_404(Post,
+                           status=Post.Status.PUBLISHED,
+                           slug=post,
+                           publish__year=year,
+                           publish__month=month,
+                           publish__day=day)
 
     return render(request,'base/post/detail.html',{'post':post})
